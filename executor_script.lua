91536121b4051db2af837778e04da6891616b34b
Kairo1414 <kairokangu160@gmail.com>
2026-02-20 22:55:49 +0200

    Update executor_script.lua to load from ice_cream_hub.lua

--- executor_script.lua
+++ executor_script.lua
@@ -1,10 +1,1 @@
--- Executor Script for Ice Cream Hub
--- This script loads and runs the Ice Cream Hub
-
-function loadIceCreamHub()
-    print("Loading Ice Cream Hub...")
-    -- Add your loading logic here
-    -- For example, required modules or scripts
-end
-
-loadIceCreamHub()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kairo1414/Ice-cream-hub/main/ice_cream_hub.lua'))()
