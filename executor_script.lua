b237d69ae62db143bb65a4bb52ffaf011d7df112
Kairo1414 <kairokangu160@gmail.com>
2026-02-20 22:54:51 +0200

    Add executor_script.lua to load and run the Ice Cream Hub

---
+++ executor_script.lua
@@ -0,0 +1,10 @@
+-- Executor Script for Ice Cream Hub
+-- This script loads and runs the Ice Cream Hub
+
+function loadIceCreamHub()
+    print("Loading Ice Cream Hub...")
+    -- Add your loading logic here
+    -- For example, required modules or scripts
+end
+
+loadIceCreamHub()
